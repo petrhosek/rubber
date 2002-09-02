a508f1343c007af28527b71dbfd0da8be9e7292b
Emmanuel Beffara <manu@beffara.org>
2002-09-02 09:00:26 +0000

    Added dependency analysis for Metapost sources.

--- src/graphics/mpost.py
+++ src/graphics/mpost.py
@@ -3,35 +3,90 @@
 """
 Metapost support for Rubber.
 
-For now, this is just a minimalistic support. It should be extended to parse
-source files for dependencies, and so on. What about a Rubber for Metapost?
+The module parses input files for dependencies and does some checkings on
+Metapost's log files after the process. Is it enough?
 """
+
+import re
 
 from rubber.util import *
 
+re_input = re.compile("input +(?P<file>[^ ]+)")
+# This is very restrictive, and so is the parsing routine. FIXME?
+
 class Dep (Depend):
+	"""
+	This class represents dependency nodes for MetaPost figures. The __init__
+	method simply creates one node for the figures and one leaf node for all
+	sources.
+	"""
 	def __init__ (self, target, source, env):
-		leaf = DependLeaf([source])
-		Depend.__init__(self, [target], {source: leaf})
+		sources = []
+		self.include(source, sources)
+		env.message(2, "%s is made from %r" % (target, sources))
+		self.leaf = DependLeaf(sources)
+		Depend.__init__(self, [target], {source: self.leaf})
 		self.env = env
 		self.base = source[:-3]
 		self.cmd = ["mpost", "--interaction=batchmode", self.base]
+		if env.process.src_path != "":
+			cmd = [
+				"env", "MPINPUTS=:%s:%s" %
+				(self.env.process.src_path, os.getenv("MPINPUTS", ""))] + cmd
+
+	def include (self, source, list):
+		"""
+		This function tries to find a specified MetaPost source (currently all
+		in the same directory), appends its actual name to the list, and
+		parses it to find recursively included files.
+		"""
+		if exists(source + ".mp"):
+			file = source + ".mp"
+		elif exists(source):
+			file = source
+		else:
+			return
+		list.append(file)
+		fd = open(file)
+		for line in file.readlines():
+			m = re_input.search(line)
+			if m:
+				self.include(m["file"])
+		fd.close()
 
 	def run (self):
 		self.env.message(0, "running Metapost on %s.mp..." % self.base)
 		self.env.process.execute(self.cmd)
+
 		# This creates a log file that has the same aspect as TeX logs.
+
 		log = open(self.base + ".log")
+		# The semantics of `error' is:
+		# 0: no error occured,
+		# 1: there has been an error,
+		# 2: there has been an error, we are showing it.
+		error = 0
 		line = log.readline()
 		while line != "":
-			if line[0] == "!":
-				self.env.message(0,
-"Metapost compilation failed (see %s.log for details)." % self.base)
-				log.close()
-				return 1
+			if error == 2:
+				self.msg(0, line.rstrip())
+				if line[0:2] == "l." or line[0:3] == "***":
+					error = 1
+			elif line[0] == "!":
+				if not error:
+					self.msg(0, _("There were errors in Metapost code:"))
+				self.msg(0, line.rstrip())
+				error = 2
 			line = log.readline()
 		log.close()
-		return 0
+		return error
+
+# The `files' dictionary associates dependency nodes to MetaPost sources. It
+# is used to detect when several figures from the same source are included. It
+# uses a global variable, and this is authentically BAD. Therefore it deserves
+# a big fat huge FIXME. Graphics modules should proably be encapsuled in
+# classes the same way as standard Rubber modules, that would help for this
+# problem.
 
 files = {}
 
