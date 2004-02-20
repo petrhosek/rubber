85f94ee116b5ed3ace606cf8804902c699db8d2f
Emmanuel Beffara <manu@beffara.org>
2004-02-20 12:56:56 +0000

    Added \\epsfbox and \\epsffile in module epsfig.

--- src/modules/epsfig.py
+++ src/modules/epsfig.py
@@ -1,5 +1,5 @@
 # This file is part of Rubber and thus covered by the GPL
-# (c) Emmanuel Beffara, 2003
+# (c) Emmanuel Beffara, 2003--2004
 """
 Epsfig support for Rubber.
 
@@ -19,6 +19,8 @@ class Module (rubber.modules.graphics.Module):
 		does that too.
 		"""
 		rubber.modules.graphics.Module.__init__(self, env, dict)
+		env.add_hook("epsfbox", self.includegraphics)
+		env.add_hook("epsffile", self.includegraphics)
 		env.add_hook("epsfig", self.epsfig)
 		env.add_hook("psfig", self.epsfig)
 
