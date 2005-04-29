4a6e6e5050cd28b33e5b2818c3bb0eab76b21e65
Emmanuel Jeandel <ejeandel@ens-lyon.fr>
2005-04-29 09:09:33 +0100

    aliases
    Add a directive alias. The syntax is the following:
    
    % rubber: alias name val
    
    now all subsequent calls to name are now treated as calls to val.
    
    As an example, after the following lines:
    % rubber: alias ig includegraphics
    \newcommand\ig[1]{\includegraphics{#1}}
    You can now use ig instead of includegraphics in your tex file and rubber
    will be aware of this.

--- src/rules/latex/__init__.py
+++ src/rules/latex/__init__.py
@@ -686,6 +686,11 @@ class LaTeXDep (Depend):
 	def do_paper (self, arg):
 		self.vars["paper"] = arg
 
+	def do_alias (self,name,val):
+	    if self.hooks.has_key(val):
+		self.hooks[name] = self.hooks[val]
+		self.update_seq()
+	    
 	def do_path (self, name):
 		self.env.path.append(expanduser(name))
 
