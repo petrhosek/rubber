a6f811ad781440b914786651f956a8449cb6ca08
Dan Liew <daniel.liew@imperial.ac.uk>
2014-05-09 15:34:31 +0100

    Made version detection a little more robust to failure. Just report UNKNOWN and issue a warning if the version information is missing.

--- rubber/version.py
+++ rubber/version.py
@@ -1,8 +1,14 @@
+import sys
 import os
 moddir = os.path.dirname(__file__)
 
 # Read version from file
-version = str(open(os.path.join(moddir, 'RELEASE-VERSION'),
-                   'r'
-                  ).readlines()[0]
-             )
+version_file = os.path.join(moddir, 'RELEASE-VERSION')
+if not os.path.exists(version_file):
+    version = 'UNKNOWN'
+    sys.stderr.write('WARNING: Version information missing.\n\n')
+else:
+    version = str(open(os.path.join(moddir, 'RELEASE-VERSION'),
+                       'r'
+                      ).readlines()[0]
+                 )
