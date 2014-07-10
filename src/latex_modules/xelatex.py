785274823ac04e17287ee01303a3c93bb65f5329
Petr Hosek <petrhosek@gmail.com>
2014-07-10 10:36:59 +0100

    Support both XDV and PDF output formats for XeTeX

--- src/latex_modules/xelatex.py
+++ src/latex_modules/xelatex.py
@@ -1,12 +1,41 @@
-from rubber import _, msg
+# This file is part of Rubber and thus covered by the GPL
+# (c) Emmanuel Beffara, 2002--2006
+"""
+XeLaTeX support for Rubber.
 
+When this module is loaded with the otion 'xdv', the document is compiled to
+XDV using XeLaTeX.
+"""
+
+from rubber import _, msg
 
 def setup(doc, context):
     doc.vars['program'] = 'xelatex'
     doc.vars['engine'] = 'XeLaTeX'
+    if 'opt' in context and context['opt'] == 'xdv':
+		mode_xdv()
+	else:
+		mode_pdf()
 
-    if doc.env.final != doc and doc.products[0][-4:] != '.pdf':
-        msg.error(_("there is already a post-processor registered"))
-        return
+def mode_pdf ():
+	global mode
+	if mode == 'pdf':
+		return
+	if doc.env.final != doc and doc.products[0][-4:] != '.pdf':
+		msg.error(_("there is already a post-processor registered"))
+		return
+	doc.reset_products([doc.target + '.pdf'])
+	doc.cmdline = [
+		opt for opt in doc.cmdline if opt != '-no-pdf']
+	mode = 'pdf'
 
-    doc.reset_products([doc.target + '.pdf'])
+def mode_xdv ():
+	global mode
+	if mode == 'xdv':
+		return
+	if doc.env.final != doc and doc.products[0][-4:] != '.xdv':
+		msg.error(_("there is already a post-processor registered"))
+		return
+	doc.reset_products([doc.target + '.xdv'])
+	doc.cmdline.insert(0, '-no-pdf')
+	mode = 'xdv'
